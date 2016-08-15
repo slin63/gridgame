395f9e2e435c7cac9002c480b814ae03729d7d02
slin63 <slin63@illinois.edu>
2016-08-14 20:22:08 -0500

    Initial Commit

---
+++ gridgame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gridgame
+//
+//  Created by SLIN63 on 8/14/16.
+//  Copyright © 2016 SLIN63. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
